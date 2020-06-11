5d38bc75992d7db84786dbe93b9efbea8a8739ab
pingyen0406 <s6622156@gmail.com>
2020-06-11 15:16:57 +0800

    Use yaml file to execute both sweep and analysis

---
+++ AtomLib/atom_sweep.py
@@ -0,0 +1,187 @@
+# 3D meta-atom TIR Library Simulation
+# This is for 800nm lattice
+# Ping-Yen 
+import S4
+import numpy as np
+import time
+import optparse
+import yaml
+import sys
+#
+class PY_S4_Config:
+    """
+    PY_S4 configuration class
+    """
+    # class variables
+    sectionName='PY_S4'
+    options={'period': (int,True),
+             'radii_range': (list,True),
+             'height_range': (list,True),
+             'wavelength': (int,True),
+             'h_alumina': (int,True),
+             'angle': (float,True),
+             'outputPath': (str,True),
+            'outputName':(str,True)}
+    #constructor
+    def __init__(self, inFileName):
+        #read YAML config and get EC_Engine section
+        infile=open(inFileName,'r')
+        ymlcfg=yaml.load(infile)
+        infile.close()
+        eccfg=ymlcfg.get(self.sectionName,None)
+        if eccfg is None: raise Exception('Missing PY_S4 section in cfg file')
+         
+        #iterate over options
+        for opt in self.options:
+            if opt in eccfg:
+                optval=eccfg[opt]
+ 
+                #verify parameter type
+                if type(optval) != self.options[opt][0]:
+                    raise Exception('Parameter "{}" has wrong type'.format(opt))
+                 
+                #create attributes on the fly
+                setattr(self,opt,optval)
+            else:
+                if self.options[opt][1]:
+                    raise Exception('Missing mandatory parameter "{}"'.format(opt))
+                else:
+                    setattr(self,opt,None)
+     
+    #string representation for class data    
+    def __str__(self):
+        return str(yaml.dump(self.__dict__,default_flow_style=False))
+
+def PY_S4(cfg):
+    outfT = open(cfg.outputPath+cfg.outputName+"_T.txt",'w')
+    outfE = open(cfg.outputPath+cfg.outputName+"_E.txt",'w')      
+    t_start = time.time() 
+    for h in range (cfg.height_range[0],cfg.height_range[1],cfg.height_range[2]):
+        print('H_atom = '+str(h)+'nm')
+        h = h/1000
+        tmid1 = time.time()
+        for hole_radius in range(cfg.radii_range[0],cfg.radii_range[1],cfg.radii_range[2]):        
+            hole_radius = hole_radius/1000
+            S=S4.New(Lattice=((cfg.period/1000,0),(0,cfg.period/1000)), NumBasis=100)
+            
+            # Material definition
+            S.SetMaterial(Name = 'Si', Epsilon = 12.0647)
+            S.SetMaterial(Name = 'a-Si', Epsilon = 12.1891)
+            # 12.0647 from Lumerical
+            # Use 11.7649 for repeating Faraon's paper, 
+            # Use 12.1891 from my ellipsometer 20191204 
+            S.SetMaterial(Name = 'Fusedsilica', Epsilon = 2.0851)
+            S.SetMaterial(Name = 'Vacuum', Epsilon = 1)
+            S.SetMaterial(Name = 'Al2O3', Epsilon=3.04852)
+                                   
+            # Layer definition
+            S.AddLayer(
+                Name = 'SiBelow', 
+                Thickness = 0, 
+                Material = 'Si')
+            S.AddLayer(
+                Name = 'Alumina_lower', 
+                Thickness = cfg.h_alumina/1000, 
+                Material = 'Al2O3')       
+            S.AddLayer(
+                Name = 'Atom', 
+                Thickness = h, 
+                Material = 'Vacuum')
+            S.AddLayer(
+                Name = 'Alumina_upper', 
+                Thickness = cfg.h_alumina/1000, 
+                Material = 'Al2O3')   
+            S.AddLayer(
+                Name = 'AirAbove', 
+                Thickness = 0, 
+                Material = 'Vacuum')
+            
+        # Incident Wave Definition
+        # Slab mode effective index=2.862458 for 500nm Al2O3, theta=37.586
+        # Slab mode effective index=2.857184 for 100nm Al2O3, theta=37.67
+        # Slab mode effective index=2.851360 for 60nm Al2O3, theta=37.76
+        # Slab mode effective index=2.846334 for 40nm Al2O3, theta=37.83
+            S.SetExcitationPlanewave(
+                IncidenceAngles=(cfg.angle,0),# (polar in [0,180],azimuthal in [0,360)
+                sAmplitude=1,
+                pAmplitude=0,
+                Order=0)        
+        # Calculation of Spectrum
+            freq = 1/cfg.wavelength*1000
+            S.SetFrequency(freq)
+        # Radius sweep loop
+          
+            S.SetRegionCircle(
+                Layer = 'Atom',
+                Material = 'a-Si',
+                Center = (0,0),
+                Radius = hole_radius)
+            # Calculation
+            Glist = S.GetBasisSet()
+            (forw_P1,back_P1) = S.GetPowerFlux(Layer = 'AirAbove', zOffset = 0)
+            (forw_P2,back_P2) = S.GetPowerFlux(Layer = 'SiBelow', zOffset = 0)
+            (forw_A,back_A) = S.GetAmplitudes(Layer = 'AirAbove', zOffset = 0)
+            totalT = forw_P1/forw_P2
+                  
+            # Finding out the index of the maximum forw_A 
+            tmp=[]
+            for ix in range(len(forw_A)):
+                #print(ix,forw_A[ix])
+                tmp.append(forw_A[ix])
+            
+            x = np.abs(tmp)
+            s = np.argmax(x)
+            outfT.write(str(totalT)+' ')
+            outfE.write(str(forw_A[s])+' ')
+            #print(s,x[s])
+            #print(totalT,forw_A[s])
+        tmid2 = time.time()-tmid1
+        print('Elasped time for this loop:',tmid2,'s')  
+        outfT.write('\n')
+        outfE.write('\n')     
+    outfT.close()
+    outfE.close()
+    t_stop=time.time()-t_start
+    print('Total elapsed time:',t_stop,'s')
+    
+
+         
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        #
+        # get command-line options
+        #
+        parser = optparse.OptionParser()
+        parser.add_option("-i", "--input", action="store", dest="inputFileName", help="input filename", default=None)
+        parser.add_option("-q", "--quiet", action="store_true", dest="quietMode", help="quiet mode", default=False)
+        parser.add_option("-d", "--debug", action="store_true", dest="debugMode", help="debug mode", default=False)
+        (options, args) = parser.parse_args(argv)
+        
+        #validate options
+        if options.inputFileName is None:
+            raise Exception("Must specify input file name using -i or --input option.")
+        #Get PY_S4 config params
+        cfg=PY_S4_Config(options.inputFileName)
+    
+        #print config params
+        print(cfg)
+                    
+        #run PY_S4
+        PY_S4(cfg)
+        
+        if not options.quietMode:                    
+            print('PY_S4 Completed!')    
+    
+    except Exception as info:
+        if 'options' in vars() and options.debugMode:
+            from traceback import print_exc
+            print_exc()
+        else:
+            print(info)
+    
+
+if __name__ == '__main__':
+    main()
+
---
+++ AtomLib/sweepAnalysis.py
@@ -0,0 +1,133 @@
+# Convert the real and imaginary to phase angle
+# This is for 800nm lattice (1 diffraction order)
+# Ping-Yen
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import os
+import sys
+import optparse
+import yaml
+from numpy import dtype
+
+class PY_S4_Analysis_Config:
+    """
+    PY_S4 configuration class
+    """
+    # class variables
+    sectionName='PY_S4_Analysis'
+    options={'height_range': (list,True),
+             'radii_range': (list,True),
+             'inputFile_T': (str,True),
+             'inputFile_E': (str,True),
+             'outputPath': (str,True),
+            'outputName':(str,True)}
+    #constructor
+    def __init__(self, inFileName):
+        #read YAML config and get EC_Engine section
+        infile=open(inFileName,'r')
+        ymlcfg=yaml.load(infile)
+        infile.close()
+        eccfg=ymlcfg.get(self.sectionName,None)
+        if eccfg is None: raise Exception('Missing PY_S4 section in cfg file')
+         
+        #iterate over options
+        for opt in self.options:
+            if opt in eccfg:
+                optval=eccfg[opt]
+ 
+                #verify parameter type
+                if type(optval) != self.options[opt][0]:
+                    raise Exception('Parameter "{}" has wrong type'.format(opt))
+                 
+                #create attributes on the fly
+                setattr(self,opt,optval)
+            else:
+                if self.options[opt][1]:
+                    raise Exception('Missing mandatory parameter "{}"'.format(opt))
+                else:
+                    setattr(self,opt,None)
+     
+    #string representation for class data    
+    def __str__(self):
+        return str(yaml.dump(self.__dict__,default_flow_style=False))
+# Unwrap electric field data
+def unwrapPhase(Edata):
+    Er = np.real(Edata)
+    Ei = np.imag(Edata)
+    theta = (np.arctan2(Ei,Er))
+    return(theta)
+    
+def PY_S4_Analysis(cfg):
+    infT = np.loadtxt(cfg.inputFile_T,dtype='complex_')
+    infE = np.loadtxt(cfg.inputFile_E,dtype='complex_')
+    
+    N_height = (cfg.height_range[1]-cfg.height_range[0])/cfg.height_range[0] 
+    N_radii =  (cfg.radii_range[1]-cfg.radii_range[0])/cfg.radii_range[0]
+    
+    
+    # Plotting transmission data
+    T=np.array(infT)
+    print(T.shape)
+    plt.figure(1)
+    plt.imshow(np.abs(T),vmin=0.00000,vmax=0.4,cmap='jet',extent=[100,450,1500,500],aspect='auto')
+    plt.colorbar()  
+    plt.savefig(cfg.outputPath+'SweepT.png')
+ 
+    # Plotting phase data
+    E=np.array(infE)
+    phase=np.zeros(shape=E.shape)
+    count=0
+    print(np.shape(E))
+    for line in E:
+        phase[count]=unwrapPhase(line)
+        count+=1
+    plt.figure(2)
+    plt.imshow(phase,cmap='jet',extent=[cfg.radii_range[0],cfg.radii_range[1],cfg.height_range[1],cfg.height_range[0]],aspect='auto')
+    plt.colorbar()
+    plt.savefig(cfg.outputPath+'SweepPhase.png')
+
+    # Save transmission & phase data
+    np.savetxt(cfg.outputPath+cfg.outputName+"_T.txt",np.abs(T))
+    np.savetxt(cfg.outputPath+cfg.outputName+"_E.txt",phase)
+    
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        #
+        # get command-line options
+        #
+        parser = optparse.OptionParser()
+        parser.add_option("-i", "--input", action="store", dest="inputFileName", help="input filename", default=None)
+        parser.add_option("-q", "--quiet", action="store_true", dest="quietMode", help="quiet mode", default=False)
+        parser.add_option("-d", "--debug", action="store_true", dest="debugMode", help="debug mode", default=False)
+        (options, args) = parser.parse_args(argv)
+        
+        #validate options
+        if options.inputFileName is None:
+            raise Exception("Must specify input file name using -i or --input option.")
+        #Get PY_S4 config params
+        cfg=PY_S4_Analysis_Config(options.inputFileName)
+    
+        #print config params
+        print(cfg)
+                    
+        #run PY_S4
+        PY_S4_Analysis(cfg)
+        
+        if not options.quietMode:                    
+            print('PY_S4_Analysis Completed!')    
+    
+    except Exception as info:
+        if 'options' in vars() and options.debugMode:
+            from traceback import print_exc
+            print_exc()
+        else:
+            print(info)
+    
+
+if __name__ == '__main__':
+    main()
+    
+    
