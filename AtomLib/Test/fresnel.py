ab1f5aada50739fcde79b8072b4523f88156cb64
PingYen <s6622156@gmail.com>
2020-12-11 16:23:10 +0800

    Add more line

--- AtomLib/Test/fresnel.py
+++ AtomLib/Test/fresnel.py
@@ -31,63 +31,77 @@ S.AddLayer(
     Thickness = 0, 
     Material = 'glass')
 
-input_wave = "p"
+input_wave = "s"
 n_glass = math.sqrt(2.08514)
 theta_i_deg = -1
-phi =180 
+phi =0 
 Tp_ana = []
 Tp=[]
-
-for i in range(90):
-    theta_i_deg+=1
-
-    print("Current input type: ",input_wave,"-wave","(theta,phi)= ("+str(theta_i_deg)+","+str(phi)+")")
-    theta_i = math.radians(theta_i_deg)
-    sin_t = math.sin(theta_i)/n_glass
-    theta_t = math.asin(sin_t)
-    if input_wave == "s":   
-        S.SetExcitationPlanewave(
-            IncidenceAngles=(theta_i_deg,phi),
-            sAmplitude=1,
-            pAmplitude=0,
-            Order=0)       
-    else:    
-        S.SetExcitationPlanewave(
-            IncidenceAngles=(theta_i_deg,phi),
-            sAmplitude=0,
-            pAmplitude=1,
-            Order=0)       
-    # Calculation of Spectrum
-    freq = 1/wavelength
-    S.SetFrequency(freq)
-    Glist = S.GetBasisSet()
-    (forw_P1,back_P1) = S.GetPowerFlux(Layer = 'Glass', zOffset = 0)
-    (forw_P2,back_P2) = S.GetPowerFlux(Layer = 'Air', zOffset = 0)
-    (forw_A,back_A) = S.GetAmplitudes(Layer = 'Glass', zOffset = 0)
-    (forw_B,back_B) = S.GetAmplitudes(Layer = 'Air',zOffset=0)
-    Trans = forw_P1/forw_P2 # Transmitance   
-    Refl = back_P2 # Reflectance
-    # Finding out the index of the maximum forw_A 
-    tmp=[]
-    for ix in range(len(forw_A)):
-        tmp.append(forw_A[ix])
-    x = np.abs(tmp)
-    s = np.argmax(x)
+Ts_ana=[]
+Ts=[]
+for i in range(2):
+    if i==0:
+        input_wave="s"
+    else:
+        input_wave="p"
+    theta_i_deg=-1
+    for j in range(91):
+        theta_i_deg+=1
     
-    # Analytical solution for S-wave
-    T_coe_s = (2*math.cos(theta_i)/(math.cos(theta_i)+n_glass*math.cos(theta_t)))
-    T_ana_s = n_glass*math.cos(theta_t)/math.cos(theta_i)*T_coe_s**2
+        print("Current input type: ",input_wave,"-wave","(theta,phi)= ("+str(theta_i_deg)+","+str(phi)+")")
+        theta_i = math.radians(theta_i_deg)
+        sin_t = math.sin(theta_i)/n_glass
+        theta_t = math.asin(sin_t)
+        if input_wave == "s":   
+            S.SetExcitationPlanewave(
+                IncidenceAngles=(theta_i_deg,phi),
+                sAmplitude=1,
+                pAmplitude=0,
+                Order=0)       
+        else:    
+            S.SetExcitationPlanewave(
+                IncidenceAngles=(theta_i_deg,phi),
+                sAmplitude=0,
+                pAmplitude=1,
+                Order=0)       
+        # Calculation of Spectrum
+        freq = 1/wavelength
+        S.SetFrequency(freq)
+        Glist = S.GetBasisSet()
+        (forw_P1,back_P1) = S.GetPowerFlux(Layer = 'Glass', zOffset = 0)
+        (forw_P2,back_P2) = S.GetPowerFlux(Layer = 'Air', zOffset = 0)
+        (forw_A,back_A) = S.GetAmplitudes(Layer = 'Glass', zOffset = 0)
+        (forw_B,back_B) = S.GetAmplitudes(Layer = 'Air',zOffset=0)
+        Trans = forw_P1/forw_P2 # Transmitance   
+        Refl = back_P2 # Reflectance
+        # Finding out the index of the maximum forw_A 
+        tmp=[]
+        for ix in range(len(forw_A)):
+            tmp.append(forw_A[ix])
+        x = np.abs(tmp)
+        s = np.argmax(x)
+        
+        # Analytical solution for S-wave
+        
+        if input_wave=="s":
     
-    T_coe_p = (2*math.cos(theta_i)/(n_glass*math.cos(theta_i)+math.cos(theta_t)))
-    T_ana_p = n_glass*math.cos(theta_t)/math.cos(theta_i)*T_coe_p**2
+            T_coe_s = (2*math.cos(theta_i)/(math.cos(theta_i)+n_glass*math.cos(theta_t)))
+            T_ana_s = n_glass*math.cos(theta_t)/math.cos(theta_i)*T_coe_s**2
+            Ts.append(Trans)
+            Ts_ana.append(T_ana_s)
+        else:
     
+            T_coe_p = (2*math.cos(theta_i)/(n_glass*math.cos(theta_i)+math.cos(theta_t)))
+            T_ana_p = n_glass*math.cos(theta_t)/math.cos(theta_i)*T_coe_p**2   
+            Tp.append(Trans)
+            Tp_ana.append(T_ana_p)
     
-    Tp.append(Trans)
-    Tp_ana.append(T_ana_p)
-
-theta_list = np.linspace(0,89,90)
+theta_list = np.linspace(0,90,91)
 plt.figure()
-plt.plot(theta_list,Tp,theta_list,Tp_ana)
+plt.plot(theta_list,Tp,theta_list,Tp_ana,theta_list,Ts,theta_list,Ts_ana)
+axes = plt.gca()
+axes.set_xlim([0,90])
+axes.set_ylim([0,1])
 plt.savefig("Test.png")
 
 #print("(forw_glass,back_glass,forw_air,back_air)=  ",forw_P1,back_P1,forw_P2,back_P2,'\n')
