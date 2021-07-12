0bfc9f470da5ab4e338f2804e8d6627f99664252
pingyen <s6622156@gmail.com>
2021-07-12 17:03:46 +0800

    Add 90 incident

--- AtomLib/Lib562/80nmSiO2/square/Square.py
+++ AtomLib/Lib562/80nmSiO2/square/Square.py
@@ -1,6 +1,5 @@
 # 3D meta-atom Library Simulation example
-# This example bases on Science Advances Vol.6m no.29, eabb4142 (17, Jul, 2020)
-# SiO2/Si/SiO2 antenna (100/400/100 nm)
+# This example bases on Nat. Commun. 6:7069 doi:10.1038/ncomms8069(2015)
 # Ping-Yen
 import S4
 import numpy as np
@@ -11,49 +10,44 @@ import math
 # Define some current path
 script_dir = os.path.abspath(os.path.dirname(__file__))
 fol_path = script_dir
-outf1 = open(os.path.join(fol_path,'Antenna_T.txt'),'w')
-outf2 = open(os.path.join(fol_path,'Antenna_E.txt'),'w')
+outf1 = open(os.path.join(fol_path,'80nmSiO2_square_T.txt'),'w')
+outf2 = open(os.path.join(fol_path,'80nmSiO2_square_E.txt'),'w')
 
 # Some parameters
-PC_period = 0.34 # period
+PC_period = 0.562 # period
 wavelength=1.55  # operating wavelength 
 t_start = time.time() # timer start
 # H_atom sweep loop
-for Lx_nm in range (100,200,25):
-    Lx = Lx_nm/1000
-    print('Lx = '+str(Lx_nm)+'nm')
+for h in range (500,1500,10):
+    print('H_atom = '+str(h)+'nm')
     tmid1 = time.time() # timer of each height loop
-    for Ly_nm in range(100,700,25):
-        Ly = Ly_nm/1000   # Change unit to um  
+    for width_nm in range(100,250,2):
+        width = width_nm/1000   # Change unit to um  
     
-        S=S4.New(Lattice=((PC_period,0),(0,PC_period)), NumBasis=100) # square lattice
+        S=S4.New(Lattice=((PC_period,0),(0,PC_period)),NumBasis=100) # square lattice
         # Note: NumBasis determines the accuracy and the computation time. 100 is a good start point.
         
         # Material definition
         S.SetMaterial(Name = 'Si', Epsilon = 12.0647) # From Lumerical
-        S.SetMaterial(Name = 'a-Si', Epsilon = 11.7649) # From paper       
+        S.SetMaterial(Name = 'a-Si', Epsilon = 12.4314) # From paper       
         S.SetMaterial(Name = 'Vacuum', Epsilon = 1)
-        S.SetMaterial(Name = 'Glass', Epsilon=2.08514)# SiO2 
+        S.SetMaterial(Name = 'glass', Epsilon=2.08514)# SiO2 
         # Material Structure Definition
-        
+        t_Si = h/1000 # thickness of silicon layer
         
         # Layer definition(from bottom to top)
         S.AddLayer(
             Name = 'Substrate', 
             Thickness = 0, 
-            Material = 'Vacuum')     
+            Material = 'Si')     
         S.AddLayer(
-            Name = 'botAtom', 
-            Thickness = 0.1, 
+            Name = 'interlayer',
+            Thickness = 0.08,
+            Material='glass')
+        S.AddLayer(
+            Name = 'Atom', 
+            Thickness = t_Si, 
             Material = 'Vacuum')
-        S.AddLayer(
-            Name = 'midAtom', 
-            Thickness = 0.4, 
-            Material = 'Vacuum')
-        S.AddLayer(
-            Name = 'topAtom', 
-            Thickness = 0.1, 
-            Material = 'Vacuum')          
         S.AddLayer(
             Name = 'AirAbove', 
             Thickness = 0, 
@@ -72,7 +66,7 @@ for Lx_nm in range (100,200,25):
         # and the angle theta specifies next the angle by which the E,H,k frame should be rotated (CCW) about the z-axis. 
         # Note the different directions of rotations for each angle.
         S.SetExcitationPlanewave(
-            IncidenceAngles=(0,90),
+            IncidenceAngles=(54.9674,90),
             sAmplitude=1,
             pAmplitude=0,
             Order=0)        
@@ -81,23 +75,11 @@ for Lx_nm in range (100,200,25):
         S.SetFrequency(freq)
     # Set the meta-atom layer have circular post    
         S.SetRegionRectangle(
-            Layer = 'botAtom',
-            Material = 'Glass',
-            Center = (0,0),
-            Angle = 0,
-            Halfwidths = (Lx/2,Ly/2))
-        S.SetRegionRectangle(
-            Layer = 'midAtom',
+            Layer = 'Atom',
             Material = 'a-Si',
             Center = (0,0),
-            Angle = 0,
-            Halfwidths = (Lx/2,Ly/2))
-        S.SetRegionRectangle(
-            Layer = 'topAtom',
-            Material = 'Glass',
-            Center = (0,0),
             Angle=0,
-            Halfwidths = (Lx/2,Ly/2))
+            Halfwidths = (width/2,width/2))
         # Pick out the transmission and the complex electric field data
         Glist = S.GetBasisSet()
         (forw_P1,back_P1) = S.GetPowerFlux(Layer = 'AirAbove', zOffset = 0)
--- AtomLib/Lib800/Lib800.py
+++ AtomLib/Lib800/Lib800.py
@@ -1,6 +1,5 @@
-# 3D meta-atom Library Simulation example
-# This example bases on Science Advances Vol.6m no.29, eabb4142 (17, Jul, 2020)
-# SiO2/Si/SiO2 antenna (100/400/100 nm)
+# 3D meta-atom Library Simulation
+# Period = 800nm
 # Ping-Yen
 import S4
 import numpy as np
@@ -11,49 +10,40 @@ import math
 # Define some current path
 script_dir = os.path.abspath(os.path.dirname(__file__))
 fol_path = script_dir
-outf1 = open(os.path.join(fol_path,'Antenna_T.txt'),'w')
-outf2 = open(os.path.join(fol_path,'Antenna_E.txt'),'w')
+outf1 = open(os.path.join(fol_path,'Example_T.txt'),'w')
+outf2 = open(os.path.join(fol_path,'Example_E.txt'),'w')
 
 # Some parameters
-PC_period = 0.34 # period
+PC_period = 0.8 # period
 wavelength=1.55  # operating wavelength 
 t_start = time.time() # timer start
 # H_atom sweep loop
-for Lx_nm in range (100,200,25):
-    Lx = Lx_nm/1000
-    print('Lx = '+str(Lx_nm)+'nm')
+for h in range (500,1200,10):
+    print('H_atom = '+str(h)+'nm')
     tmid1 = time.time() # timer of each height loop
-    for Ly_nm in range(100,700,25):
-        Ly = Ly_nm/1000   # Change unit to um  
+    for hole_radius_nm in range(100,350,2):
+        hole_radius = hole_radius_nm/1000   # Change unit to um  
     
         S=S4.New(Lattice=((PC_period,0),(0,PC_period)), NumBasis=100) # square lattice
         # Note: NumBasis determines the accuracy and the computation time. 100 is a good start point.
         
         # Material definition
         S.SetMaterial(Name = 'Si', Epsilon = 12.0647) # From Lumerical
-        S.SetMaterial(Name = 'a-Si', Epsilon = 11.7649) # From paper       
+        S.SetMaterial(Name = 'a-Si', Epsilon = 3.52**2) # From Ellipsometer       
         S.SetMaterial(Name = 'Vacuum', Epsilon = 1)
-        S.SetMaterial(Name = 'Glass', Epsilon=2.08514)# SiO2 
+        S.SetMaterial(Name = 'glass', Epsilon=2.08514)# SiO2 
         # Material Structure Definition
-        
+        t_Si = h/1000 # thickness of silicon layer
         
         # Layer definition(from bottom to top)
         S.AddLayer(
             Name = 'Substrate', 
             Thickness = 0, 
-            Material = 'Vacuum')     
+            Material = 'Si')     
         S.AddLayer(
-            Name = 'botAtom', 
-            Thickness = 0.1, 
+            Name = 'Atom', 
+            Thickness = t_Si, 
             Material = 'Vacuum')
-        S.AddLayer(
-            Name = 'midAtom', 
-            Thickness = 0.4, 
-            Material = 'Vacuum')
-        S.AddLayer(
-            Name = 'topAtom', 
-            Thickness = 0.1, 
-            Material = 'Vacuum')          
         S.AddLayer(
             Name = 'AirAbove', 
             Thickness = 0, 
@@ -72,7 +62,7 @@ for Lx_nm in range (100,200,25):
         # and the angle theta specifies next the angle by which the E,H,k frame should be rotated (CCW) about the z-axis. 
         # Note the different directions of rotations for each angle.
         S.SetExcitationPlanewave(
-            IncidenceAngles=(0,90),
+            IncidenceAngles=(54.9674,90),
             sAmplitude=1,
             pAmplitude=0,
             Order=0)        
@@ -80,24 +70,11 @@ for Lx_nm in range (100,200,25):
         freq = 1/wavelength
         S.SetFrequency(freq)
     # Set the meta-atom layer have circular post    
-        S.SetRegionRectangle(
-            Layer = 'botAtom',
-            Material = 'Glass',
-            Center = (0,0),
-            Angle = 0,
-            Halfwidths = (Lx/2,Ly/2))
-        S.SetRegionRectangle(
-            Layer = 'midAtom',
+        S.SetRegionCircle(
+            Layer = 'Atom',
             Material = 'a-Si',
             Center = (0,0),
-            Angle = 0,
-            Halfwidths = (Lx/2,Ly/2))
-        S.SetRegionRectangle(
-            Layer = 'topAtom',
-            Material = 'Glass',
-            Center = (0,0),
-            Angle=0,
-            Halfwidths = (Lx/2,Ly/2))
+            Radius = hole_radius)
         # Pick out the transmission and the complex electric field data
         Glist = S.GetBasisSet()
         (forw_P1,back_P1) = S.GetPowerFlux(Layer = 'AirAbove', zOffset = 0)
