caf37a403060926890e3db7280f4d48363df754c
pingyen0406 <s6622156@gmail.com>
2021-09-15 01:07:54 +0800

    Run modeTest again

--- AtomLib/Lib562/80nmSiO2/modeTest/modeAnalysis.py
+++ AtomLib/Lib562/80nmSiO2/modeTest/modeAnalysis.py
@@ -8,7 +8,8 @@ import math
 # radii_range: [50,250,2]
 # height_range: [300,3000,30]
 
-infName = "modeTest0_T.txt"
+#infName = "modeTest0_T.txt"
+infName = "modeTestTIR_T.txt"
 T_all = np.loadtxt(infName)
 print(T_all.shape)
 hList = np.array([i for i in range(300,3000,30)])
@@ -27,6 +28,6 @@ plt.plot(hList,TList100,linewidth=2,label='radius=100')
 plt.plot(hList,TList150,linewidth=2,label='radius=150')
 plt.xlabel('height(nm)')
 plt.legend()
-plt.savefig('0_mode.png')
+#plt.savefig('0_mode.png')
 plt.show()
 
